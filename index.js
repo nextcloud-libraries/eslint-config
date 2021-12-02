127f3f888d1988c08d06cf4f991ace9a52f5923b
John Molakvoæ <skjnldsv@users.noreply.github.com>
2021-12-02 18:12:49 +0100

    Disable tag rule
    See https://github.com/gajus/eslint-plugin-jsdoc/issues/782#issuecomment-918272790

--- index.js
+++ index.js
@@ -62,11 +62,7 @@ module.exports = {
 		// force proper JSDocs
 		'jsdoc/require-returns': 0,
 		'jsdoc/require-returns-description': 0,
-		'jsdoc/tag-lines': ['warn', 'never', { count: 0, tags: {
-			copyright: { count: 1, lines: 'always' },
-			author: { count: 1, lines: 'always' },
-			license: { count: 1, lines: 'always' },
-		}}],
+		'jsdoc/tag-lines': ['off'],
 		// disallow use of "var"
 		'no-var': 'error',
 		// suggest using const
