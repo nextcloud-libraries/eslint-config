90aa2bb9de03b611c9933b68d60cb5a03b158357
Ferdinand Thiessen <opensource@fthiessen.de>
2024-05-15 16:01:42 +0200

    feat: Add `vue3` sub-configuration to allow linting Vue 3 projects
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ parts/vue3.js
@@ -0,0 +1,39 @@
+module.exports = {
+	files: ['**/*.vue'],
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		parser: '@babel/eslint-parser',
+	},
+	extends: ['plugin:vue3/recommended'],
+	rules: {
+		'vue/html-indent': ['error', 'tab'],
+		// PascalCase components names for vuejs
+		// https://vuejs.org/style-guide/rules-strongly-recommended.html#component-name-casing-in-templates
+		'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+		// force name
+		'vue/match-component-file-name': ['error', {
+			extensions: ['jsx', 'vue', 'js', 'ts', 'tsx'],
+			shouldMatchCase: true,
+		}],
+		// space before self-closing elements
+		'vue/html-closing-bracket-spacing': 'error',
+		// no ending html tag on a new line
+		'vue/html-closing-bracket-newline': ['error', { multiline: 'never' }],
+		// check vue files too
+		'n/no-missing-import': ['error', {}],
+		// code spacing with attributes
+		'vue/max-attributes-per-line': ['error', {
+			singleline: 3,
+			multiline: 1,
+		}],
+		// See https://vuejs.org/style-guide/rules-strongly-recommended.html#multi-attribute-elements
+		'vue/first-attribute-linebreak': ['error', {
+			singleline: 'beside',
+			multiline: 'below',
+		}],
+		// Prevent conflicts with native HTML elements
+		'vue/multi-word-component-names': 'error',
+		// custom event naming convention
+		'vue/custom-event-name-casing': 'warning',
+	},
+}
