b0ca47e091880bbd1f359e65b584bf8f266721b2
Ferdinand Thiessen <opensource@fthiessen.de>
2025-04-09 11:09:10 +0200

    refactor: import `package.json` instead of using built-time version generation
    This needs us to bump Node version to at least 20.10 to support import
    types.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/version.ts
+++ lib/version.ts
@@ -2,6 +2,6 @@
  * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+import packageJson from '../package.json' with { type: 'json' }
 
-// this will be replaced on built-time
-export const __PACKAGE_VERSION__: string = '0.0.0'
+export const __PACKAGE_VERSION__: string = packageJson.version
