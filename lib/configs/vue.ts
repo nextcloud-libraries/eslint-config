9f964997c0c14973e8ac23597cf96d10b80d98f4
Ferdinand Thiessen <opensource@fthiessen.de>
2025-06-05 00:47:57 +0200

    feat(vue): add `vue/prefer-separate-static-class` rule
    A more stylistic rule to enforce what we already soft-enforce (review
    based) on e.g. nextcloud-vue: Use separate static class attribute for
    plain class and dynamic class attribute if needed.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/configs/vue.ts
+++ lib/configs/vue.ts
@@ -129,6 +129,8 @@ export function vue(options: ConfigOptions): Linter.Config[] {
 				'vue/new-line-between-multi-line-property': 'error',
 				// Add consistent padding between blocks
 				'vue/padding-line-between-blocks': 'error',
+				// prefer separated static and dynamic class attributes
+				'vue/prefer-separate-static-class': 'error',
 			},
 			name: 'nextcloud/vue/stylistic-rules',
 		},
